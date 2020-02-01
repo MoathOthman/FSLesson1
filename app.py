9348bada96a88da932655ae7c0deab7fdb658508
moath othman <dark2torch@gmail.com>
2020-02-01 21:00:18 +0300

    end of lessons 6 migration

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, abort, render_template, request, redirect, url_for,jsonify
 import sys
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/moath'
@@ -8,15 +9,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model): 
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
-
-db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
